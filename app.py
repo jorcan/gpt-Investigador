c7e3807c6a06257150bfc896e488122336b591ba
Jorge <jorcan46@gmail.com>
2023-08-09 21:50:21 +0200

    version inicial operativa

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-
+import streamlit as st
 from langchain import PromptTemplate
 from langchain.agents import initialize_agent, Tool
 from langchain.agents import AgentType
@@ -16,63 +16,50 @@ from bs4 import BeautifulSoup
 import requests
 import json
 from langchain.schema import SystemMessage
-from fastapi import FastAPI
 
+# Cargar las variables de entorno desde el archivo .env
 load_dotenv()
-brwoserless_api_key = os.getenv("BROWSERLESS_API_KEY")
+browserless_api_key = os.getenv("BROWSERLESS_API_KEY")
 serper_api_key = os.getenv("SERP_API_KEY")
 
-# 1. Tool for search
-
-
+# 1. Herramienta para búsqueda
 def search(query):
     url = "https://google.serper.dev/search"
-
     payload = json.dumps({
         "q": query
     })
-
+    print(payload)
     headers = {
         'X-API-KEY': serper_api_key,
         'Content-Type': 'application/json'
     }
-
     response = requests.request("POST", url, headers=headers, data=payload)
-
-    print(response.text)
+    
+    print(response)
 
     return response.text
+    
 
 
-# 2. Tool for scraping
+# 2. Herramienta para scraping
 def scrape_website(objective: str, url: str):
-    # scrape website, and also will summarize the content based on objective if the content is too large
-    # objective is the original objective & task that user give to the agent, url is the url of the website to be scraped
-
-    print("Scraping website...")
-    # Define the headers for the request
+    # Extraer contenido de un sitio web y resumirlo si es necesario
     headers = {
         'Cache-Control': 'no-cache',
         'Content-Type': 'application/json',
     }
 
-    # Define the data to be sent in the request
     data = {
         "url": url
     }
 
-    # Convert Python object to JSON string
     data_json = json.dumps(data)
-
-    # Send the POST request
-    post_url = f"https://chrome.browserless.io/content?token={brwoserless_api_key}"
+    post_url = f"https://chrome.browserless.io/content?token={browserless_api_key}"
     response = requests.post(post_url, headers=headers, data=data_json)
 
-    # Check the response status code
     if response.status_code == 200:
         soup = BeautifulSoup(response.content, "html.parser")
         text = soup.get_text()
-        print("CONTENTTTTTT:", text)
 
         if len(text) > 10000:
             output = summary(objective, text)
@@ -82,7 +69,7 @@ def scrape_website(objective: str, url: str):
     else:
         print(f"HTTP request failed with status code {response.status_code}")
 
-
+# 3. Resumen del contenido
 def summary(objective, content):
     llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo-16k-0613")
 
@@ -90,9 +77,9 @@ def summary(objective, content):
         separators=["\n\n", "\n"], chunk_size=10000, chunk_overlap=500)
     docs = text_splitter.create_documents([content])
     map_prompt = """
-    Write a summary of the following text for {objective}:
+    Escribe un resumen del siguiente texto para {objective}:
     "{text}"
-    SUMMARY:
+    RESUMEN:
     """
     map_prompt_template = PromptTemplate(
         template=map_prompt, input_variables=["text", "objective"])
@@ -109,49 +96,50 @@ def summary(objective, content):
 
     return output
 
-
+# Definir la estructura de entrada para la herramienta de scraping
 class ScrapeWebsiteInput(BaseModel):
-    """Inputs for scrape_website"""
+    """Entradas para la función de scraping"""
     objective: str = Field(
-        description="The objective & task that users give to the agent")
-    url: str = Field(description="The url of the website to be scraped")
+        description="El objetivo y tarea que los usuarios dan al agente")
+    url: str = Field(description="La URL del sitio web a ser escrapeado")
 
-
+# Definir la herramienta de scraping
 class ScrapeWebsiteTool(BaseTool):
     name = "scrape_website"
-    description = "useful when you need to get data from a website url, passing both url and objective to the function; DO NOT make up any url, the url should only be from the search results"
+    description = "Útil para obtener datos de una URL de un sitio web y, posiblemente, resumir el contenido si es largo."
     args_schema: Type[BaseModel] = ScrapeWebsiteInput
 
     def _run(self, objective: str, url: str):
         return scrape_website(objective, url)
 
     def _arun(self, url: str):
-        raise NotImplementedError("error here")
+        raise NotImplementedError("error aquí")
 
-
-# 3. Create langchain agent with the tools above
+# Crear una lista de herramientas para el agente
 tools = [
     Tool(
-        name="Search",
+        name="Busqueda",
         func=search,
-        description="useful for when you need to answer questions about current events, data. You should ask targeted questions"
+        description="Útil para responder preguntas sobre eventos actuales y datos. Hacer preguntas específicas es recomendado."
     ),
     ScrapeWebsiteTool(),
 ]
 
+# Definir el mensaje del sistema para el agente
 system_message = SystemMessage(
-    content="""You are a world class researcher, who can do detailed research on any topic and produce facts based results; 
-            you do not make things up, you will try as hard as possible to gather facts & data to back up the research
+    content="""Eres un investigador de clase mundial, capaz de realizar investigaciones detalladas sobre cualquier tema y producir resultados basados en hechos; no inventas cosas, te esfuerzas al máximo para recopilar hechos y datos que respalden la investigación.
             
-            Please make sure you complete the objective above with the following rules:
-            1/ You should do enough research to gather as much information as possible about the objective
-            2/ If there are url of relevant links & articles, you will scrape it to gather more information
-            3/ After scraping & search, you should think "is there any new things i should search & scraping based on the data I collected to increase research quality?" If answer is yes, continue; But don't do this more than 3 iteratins
-            4/ You should not make things up, you should only write facts & data that you have gathered
-            5/ In the final output, You should include all reference data & links to back up your research; You should include all reference data & links to back up your research
-            6/ In the final output, You should include all reference data & links to back up your research; You should include all reference data & links to back up your research"""
+            Asegúrate de cumplir el objetivo anterior con las siguientes reglas:
+
+1/ Debes realizar suficiente investigación para recopilar la mayor cantidad posible de información sobre el objetivo.
+2/ Si hay URLs de enlaces y artículos relevantes, los recopilarás para obtener más información.
+3/ Después de recopilar y buscar información, debes preguntarte "¿hay cosas nuevas que debería buscar y recopilar en función de los datos que he recolectado para mejorar la calidad de la investigación?" Si la respuesta es sí, continúa; pero no hagas esto más de 3 iteraciones.
+4/ No debes inventar cosas, solo debes escribir hechos y datos que hayas recopilado.
+5/ En el resultado final, debes incluir todos los datos de referencia y enlaces para respaldar tu investigación.
+7/ En el resultado final, debes incluir todos los datos de referencia y enlaces para respaldar tu investigación; debes incluir todos los datos de referencia y enlaces para respaldar tu investigación."""
 )
 
+# Configuraciones adicionales para el agente
 agent_kwargs = {
     "extra_prompt_messages": [MessagesPlaceholder(variable_name="memory")],
     "system_message": system_message,
@@ -159,8 +147,9 @@ agent_kwargs = {
 
 llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo-16k-0613")
 memory = ConversationSummaryBufferMemory(
-    memory_key="memory", return_messages=True, llm=llm, max_token_limit=1000)
+    memory_key="memory", return_messages=True, llm=llm, max_token_limit=2000)
 
+# Inicializar el agente
 agent = initialize_agent(
     tools,
     llm,
@@ -170,28 +159,43 @@ agent = initialize_agent(
     memory=memory,
 )
 
+# 4. Usar Streamlit para crear una aplicación web
+def main():
+    
+    # Configuración de la página y título
+    st.set_page_config(page_title="AI BrainQ Agente", page_icon=":male_detective:")
+    # Cargar la imagen del logo
+    image_path = os.path.join(os.path.dirname(__file__), "static/images/brainq-logo.png")
+    st.image(image_path, width=200)    
+    
 
-# 4. Use streamlit to create a web app
-# def main():
-#     st.set_page_config(page_title="AI research agent", page_icon=":bird:")
+    # Encabezado y entrada de texto
+    st.header("AI Agente de investigación 🕵🏻")
+    query = st.text_input("Objetivo de investigación")
 
-#     st.header("AI research agent :bird:")
-#     query = st.text_input("Research goal")
+    if query:
+        st.write("Estoy investigando...   ", query)
 
-#     if query:
-#         st.write("Doing research for ", query)
+        # Llamar al agente
+        result = agent({"input": query})
 
-#         result = agent({"input": query})
+        # Mostrar resultado
+        if "output" in result:
+            st.info(result['output'])
+        else:
+            st.error("No se encontró el resultado esperado en la respuesta del agente.")
 
-#         st.info(result['output'])
+if __name__ == '__main__':
+    # Definir el puerto 8080 para Streamlit
+    #port = 8080
+    #st.set_option("server.port", port)
+
+    # Ejecutar la función principal
+    main()
 
 
-# if __name__ == '__main__':
-#     main()
-
-
-# 5. Set this as an API endpoint via FastAPI
-app = FastAPI()
+#5. Set this as an API endpoint via FastAPI
+''' app = FastAPI()
 
 
 class Query(BaseModel):
@@ -203,4 +207,4 @@ def researchAgent(query: Query):
     query = query.query
     content = agent({"input": query})
     actual_content = content['output']
-    return actual_content
+    return actual_content '''
